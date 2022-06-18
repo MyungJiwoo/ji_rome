7483731db22d544519190be30092967d0255ebc6
mongdiwoo <1206jiwoo@gmail.com>
2022-06-18 16:38:08 +0900

    0618

--- js/weather.js
+++ js/weather.js
@@ -12,7 +12,7 @@ function onGeoOk(position) {
             const city = document.querySelector("#weather span:first-child");
             const weather = document.querySelector("#weather span:last-child");
             city.innerText = `@ ${data.name} / `;
-            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+            weather.innerText = `/ ${data.weather[0].main}, ${data.main.temp}°C`;
         }); // js가 url을 부름 - console의 network에서 문서 확인 가능
 }
 
